de76e3a684940949dcb169ea52219d66e0d28ff1
gizem hazal akbulut <gizemhazalakbulut@hotmail.com>
2025-06-09 22:38:18 +0300

    Add project files.

---
+++ BlogStore.DataAccessLayer/Context/BlogContext.cs
@@ -0,0 +1,23 @@
+﻿using BlogStore.EntityLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogStore.DataAccessLayer.Context
+{
+    internal class BlogContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=;initial catalog=BlogStoreNightDb;integrated Security=true;trust server certificate=true");
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+    }
+}
---
+++ BlogStore.EntityLayer/Entities/Tag.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogStore.EntityLayer.Entities
+{
+    public class Tag // Etiketler
+    {
+        public int TagId { get; set; }
+        public string Title { get; set; }
+    }
+}
+
+// Etiket bulutu, bir blog yazısı yazdık onun etiket bulutu hastag mantıgında yazıyı öne çıkaracak etiketler oluyor. 
