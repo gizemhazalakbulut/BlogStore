162558bc8ea3fb093d9d5ae24595af348502052c
gizem hazal akbulut <gizemhazalakbulut@hotmail.com>
2025-08-25 00:24:22 +0300

    .

--- BlogStore.PresentationLayer/Controllers/RegisterController.cs
+++ BlogStore.PresentationLayer/Controllers/RegisterController.cs
@@ -1,5 +1,6 @@
 ﻿using BlogStore.EntityLayer.Entities;
 using BlogStore.PresentationLayer.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Storage;
@@ -15,15 +16,20 @@ namespace BlogStore.PresentationLayer.Controllers
             _userManager = userManager;
         }
 
-
+        [HttpGet]
+        [AllowAnonymous]
         public IActionResult CreateUser()
         {
             return View();
         }
 
         [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
         public async Task <IActionResult> CreateUser(UserRegisterViewModel userRegisterViewModel)
         {
+            if (!ModelState.IsValid) return View(userRegisterViewModel);
+
             AppUser appUser = new AppUser
             {
                 Name = userRegisterViewModel.Name,
@@ -34,8 +40,18 @@ namespace BlogStore.PresentationLayer.Controllers
                 Description = "test" // Default description, can be changed later
 
             };
-            await _userManager.CreateAsync(appUser, userRegisterViewModel.Password);
-            return RedirectToAction("UserLogin", "Login");
+           var result = await _userManager.CreateAsync(appUser, userRegisterViewModel.Password);
+            if (result.Succeeded)
+            {
+                // İstersen otomatik giriş yap:
+                // await _signInManager.SignInAsync(appUser, isPersistent: true);
+                return RedirectToAction("UserLogin", "Login");
+            }
+
+            foreach (var e in result.Errors)
+                ModelState.AddModelError(string.Empty, e.Description);
+
+            return View(userRegisterViewModel);
         }
     }
 }
